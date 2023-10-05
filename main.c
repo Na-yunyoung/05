4ce8dad09bf2af9ee12ba230191268e684852fc0
Na-yunyoung <yunyoung23@sookmyung.ac.kr>
2023-10-05 16:02:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,24 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
 	int a;
 	
 	printf("정수 하나를 입력하시오 : ");
-	scanf("%i", &a);
+	scanf("%i", &a );
 	
-	if (a > 0)
-	{
-		printf("양수입니다.");
-	}
-	else if (a == 0)
-	{
-		printf("0 입니다.");
-	}
-	else 
-	{
-		printf("음수입니다.");
-	}
+	if (a < 0 )
+		a = -a;
+		
+	//else
+	
+	printf("절대값은 %i 입니다.", a);
 	
 	return 0;
 }
